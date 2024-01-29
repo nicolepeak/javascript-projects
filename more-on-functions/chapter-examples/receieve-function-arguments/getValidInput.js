f2c7462b7b7d7f918207dfc9087dd2aa8d91dc16
Nicole Peak <colecpeak@gmail.com>
2024-01-29 09:30:05 -0600

    ch 11 exercises

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -14,8 +14,23 @@ function getValidInput(prompt, isValid) {
 
 // TODO 1: write a validator 
 // that ensures input starts with "a"
+doesStartWithA = function (string) {
+  if (string[0].toLowerCase() === "a") {
+    return true
+  }
+
+};
+
 
 // TODO 2: write a validator 
 // that ensures input is a vowel
+//doesStartWithVowel = function (string) {
+ // if (string[0] == "a" || "e"|| "i" || "o" || "u") {
+  //  return true
+ // }
+//}
+
 
 // Be sure to test your code!
+//console.log(getValidInput('Enter a valid word: ', doesStartWithA));
+//console.log (getValidInput('Enter a name: ', doesStartWithVowel));
