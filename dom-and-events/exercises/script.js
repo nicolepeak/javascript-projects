5357142834acc51d086b0877728a3017e21b345c
Nicole Peak <colecpeak@gmail.com>
2024-02-26 13:52:32 -0600

    DOM exercises done

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,35 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+
+  missionAbort.addEventListener("mouseover", (event) => {
+    missionAbort.style.cursor = "red";
+  });
+
+  missionAbort.addEventListener("mouseover", () => {
+    missionAbort.style.backgroundColor = "red";
+  });
+
+  missionAbort.addEventListener("mouseout", () => {
+    missionAbort.style.backgroundColor = "";
+  });
+
+  missionAbort.addEventListener("click", () => {
+    const userConfirmed = window.confirm(
+      "Are you sure you want to abort the mission?"
+    );
+
+    if (userConfirmed) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+    }
+  });
 }
 
 window.addEventListener("load", init);
+
+script.js;
