0dabc1a695a5e06047c89ad595464a03e0c6704e
Nicole Peak <colecpeak@gmail.com>
2024-02-29 13:34:28 -0600

    form exercises done

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,11 @@
-//Code Your Solution Below
+window.addEventListener("load", () => {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    let testname = document.querySelector("input[name=testName]");
+    if (testname.value === "") {
+      alert("All fields are required!");
+      // stop the form submission
+      event.preventDefault();
+    }
+  });
+});
