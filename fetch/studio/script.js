f7c98b8e5add51fed995d94bbbba4ab8dab9d26b
Nicole Peak <colecpeak@gmail.com>
2024-03-04 19:59:59 -0600

    fetch studio complete

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,26 @@
-//TODO: Add Your Code Below
+window.addEventListener("load", function () {
+  fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonRes) {
+      const container = document.querySelector("#container");
+      let astronauts = "";
+      for (const astronaut of jsonRes) {
+        astronauts += `
+            <div class="astronaut">
+              <div class="bio">
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                <ul>
+                  <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                  <li>Active: ${astronaut.active}</li>
+                  <li>Skills: ${astronaut.skills.join(", ")}</li>
+                </ul>
+              </div>
+              <img class="avatar" src="${astronaut.picture}">
+            </div>
+          `;
+        container.innerHTML = astronauts;
+      }
+    });
+});
